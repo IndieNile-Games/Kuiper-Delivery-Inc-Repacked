04c54b710723b2548da2791bbf0048716d6259e9
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-11-30 00:43:44 -0600

    Added ship animations

--- scenes/player/player_ship.gd
+++ scenes/player/player_ship.gd
@@ -1,23 +1,19 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0
+const LERP = 4.5;
+
+func _ready() -> void:
+	$AnimTree.set("parameters/blend_position", 0) # Initiates the Animation Tree
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and not is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * SPEED, LERP)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, LERP/2)
 
-	move_and_slide()
+	$AnimTree.set("parameters/blend_position", velocity.x) # Updates the animator
+	move_and_slide() # Slides us around.
